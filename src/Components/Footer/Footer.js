2fa22e2d118091a656bce640713a3837c20f8c2f
krAlena <krasnoshchoka@bk.ru>
2024-08-30 19:08:03 +0300

    footer done, feader style fixes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -18,13 +18,45 @@ const Footer = () => {
                             <div className='info big'>
                                 Want to recieve our awesome stories?
                             </div>
+                            <div className='input-with-button flex-row'>
+                                <input className='subscribe-input' placeholder='Enter your email'/>
+                                <div className='button black btn-subscribe without-margin'>Subscribe</div>
+                            </div>
                         </div>
                     </div>
                     <div className='bottom-block description white'>
                         © The 18 Design, All rights reserved.
                     </div>
                 </div>
-                <div className='secondary-block'></div>
+                <div className='secondary-block flex-col space-between'>
+                    <div className='info-cols-parent space-between flex-row'>
+                        <div className='info-col flex-col'>
+                            <div className='description white email-title'>Email</div>
+                            <div className='description white'>info@the18.design</div>
+                        </div>
+                        <div className='info-col flex-col'>
+                            <div className='description white'>About us</div>
+                            <div className='description white'>How it works</div>
+                            <div className='description white'>Pricing</div>
+                            <div className='description white'>FAQs</div>
+                        </div>
+                        <div className='info-col flex-col'>
+                            <div className='description white'>Lead generation</div>
+                            <div className='description white'>Customer engagement</div>
+                            <div className='description white'>Customer support</div>
+                            <div className='description white'>Help Center Articles</div>
+                            <div className='description white'>Outbound Messages</div>
+                            
+                        </div>
+                    </div>
+                    <div className='icons-panel flex-row'>
+                        <img className="logo" src="/img/socialmedia/social_fb.svg" alt="facebook logo"/>
+                        <img className="logo" src="/img/socialmedia/social_tw.svg" alt="twitter logo"/>
+                        <img className="logo" src="/img/socialmedia/social_gl.svg" alt="google logo"/>
+                        <img className="logo" src="/img/socialmedia/social_in.svg" alt="linkedin logo"/>
+                        <img className="logo" src="/img/socialmedia/social_yt.svg" alt="youtube logo"/>
+                    </div>
+                </div>
             </div>
         </div>
     );
