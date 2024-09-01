bdcfbdb51c7c476c5011c7da17b9e3247085b9fe
krAlena <krasnoshchoka@bk.ru>
2024-09-01 13:32:58 +0300

    FAQs: separate Question Component

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -2,8 +2,36 @@ import React, {useState, useEffect} from "react";
 import {Element} from "react-scroll";
 import "./FAQ.sass";
 import { FAQ_ID } from "../../Constants/routes";
+import QuestionBlock from "./QuestionBlock";
 
 const FAQ = () => {
+    const arrQuestions = [
+        {
+            _id: 0,
+            title: "How much does a Teams subscription cost?",
+            answer: "Contributors are members of your team who need access to use the Mac app to create and edit Sketch documents. These prices don’t include sales tax, which may still apply. Head over to our pricing page to get full details."
+        },
+        {
+            _id: 1,
+            title: "Do I still need to purchase licenses for the Mac app?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id viverra nibh eros, aliquet purus molestie eu convallis. Pellentesque eu egestas lectus integer sollicitudin odio. Quis lectus ante at lacus ut. Pretium fermentum ac convallis pharetra lacus nibh at sit est. Sit donec nulla nisl, praesent est egestas faucibus ullamcorper curabitur."
+        },
+        {
+            _id: 2,
+            title: "What are the differences between Contributors and Viewers?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id viverra nibh eros, aliquet purus molestie eu convallis."
+        },
+        {
+            _id: 3,
+            title: "How long does the free Teams subscription trial last?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id viverra nibh eros, aliquet purus molestie eu convallis. Pellentesque eu egestas lectus integer sollicitudin odio. Quis lectus ante at lacus ut. Pretium fermentum ac convallis pharetra lacus nibh at sit est. Sit donec nulla nisl, praesent est egestas faucibus ullamcorper curabitur. Fringilla mattis blandit felis cursque."
+        },
+        {
+            _id: 4,
+            title: "Will program for Teams replace volume licensing?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id viverra nibh eros, aliquet purus molestie eu convallis. Pellentesque eu egestas lectus integer sollicitudin odio. Quis lectus ante at lacus ut. Pretium fermentum ac convallis pharetra lacus nibh at sit est. Sit donec nulla nisl, praesent est egestas faucibus ullamcorper curabitur."
+        }
+    ]
 
     return (
         <Element name={FAQ_ID}>
@@ -22,61 +50,12 @@ const FAQ = () => {
                             </div>
                         </div>
                         <div className="questions-list">
-                            <div className="question-block flex-row full-width space-between">
-                                <div className="question-body flex-col">
-                                    <div className="question-title">How much does a Teams subscription cost?</div>
-                                    <div className="question-answer">
-                                        Contributors are members of your team who need access to use
-                                        the Mac app to create and edit Sketch documents. These prices don’t include sales
-                                        tax, which may still apply. Head over to our pricing page to get full details.
-                                    </div>
-                                </div>
-                                <img className="icon btn" src="/img/icons/plus.svg" alt="plus"/>
-                            </div>
-                            <div className="question-block flex-row full-width space-between">
-                                <div className="question-body flex-col">
-                                    <div className="question-title">Do I still need to purchase licenses for the Mac app?</div>
-                                    {/* <div className="question-answer">
-                                        Contributors are members of your team who need access to use
-                                        the Mac app to create and edit Sketch documents. These prices don’t include sales
-                                        tax, which may still apply. Head over to our pricing page to get full details.
-                                    </div> */}
-                                </div>
-                                <img className="icon btn" src="/img/icons/minus.svg" alt="minus"/>
-                            </div>
-                            <div className="question-block flex-row full-width space-between">
-                                <div className="question-body flex-col">
-                                    <div className="question-title">What are the differences between Contributors and Viewers?</div>
-                                    {/* <div className="question-answer">
-                                        Contributors are members of your team who need access to use
-                                        the Mac app to create and edit Sketch documents. These prices don’t include sales
-                                        tax, which may still apply. Head over to our pricing page to get full details.
-                                    </div> */}
-                                </div>
-                                <img className="icon btn" src="/img/icons/minus.svg" alt="minus"/>
-                            </div>
-                            <div className="question-block flex-row full-width space-between">
-                                <div className="question-body flex-col">
-                                    <div className="question-title">How long does the free Teams subscription trial last?</div>
-                                    {/* <div className="question-answer">
-                                        Contributors are members of your team who need access to use
-                                        the Mac app to create and edit Sketch documents. These prices don’t include sales
-                                        tax, which may still apply. Head over to our pricing page to get full details.
-                                    </div> */}
-                                </div>
-                                <img className="icon btn" src="/img/icons/minus.svg" alt="minus"/>
-                            </div>
-                            <div className="question-block flex-row full-width space-between">
-                                <div className="question-body flex-col">
-                                    <div className="question-title">Will program for Teams replace volume licensing?</div>
-                                    {/* <div className="question-answer">
-                                        Contributors are members of your team who need access to use
-                                        the Mac app to create and edit Sketch documents. These prices don’t include sales
-                                        tax, which may still apply. Head over to our pricing page to get full details.
-                                    </div> */}
-                                </div>
-                                <img className="icon btn" src="/img/icons/minus.svg" alt="minus"/>
-                            </div>
+                            {
+                                Array.isArray(arrQuestions) && arrQuestions.length > 0
+                                    ?   arrQuestions.map((el,index) => <QuestionBlock isOpened={(index===0)} questionObj={el}/>)
+                                    :   null
+                            }
+
                         </div>
                     </div>
                     <div className="illustration-part">
---
+++ src/Components/FAQ/QuestionBlock.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from "react";
+import "./QuestionBlock.sass";
+
+const QuestionBlock = ({isOpened, questionObj}) => {
+    const [isVisibleAnswer, setIsVisibleAnswer] = useState(isOpened);
+
+    return (
+        <div key={"question_" + questionObj._id} className="question-block flex-row full-width space-between">
+            <div className="question-body flex-col">
+                <div className="question-title">{questionObj.title}</div>
+                <div className={!isVisibleAnswer ? "question-answer hidden" : "question-answer"}>
+                    {questionObj.answer}
+                </div>
+            </div>
+            <img className="icon btn"
+                onClick={() => setIsVisibleAnswer(!isVisibleAnswer)}
+                src={isVisibleAnswer ? "/img/icons/minus.svg" : "/img/icons/plus.svg"}
+                alt={isVisibleAnswer ? "minus" : "plus"}
+            />
+        </div>
+    );
+};
+
+export default QuestionBlock;
