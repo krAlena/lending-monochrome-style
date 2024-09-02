665b05d6ad7be86f18fe91a7ea0d7f4710cd38f6
krAlena <krasnoshchoka@bk.ru>
2024-09-02 22:05:44 +0300

    CTA mob view

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,8 +2,10 @@ import React, {useState, useEffect} from "react";
 import "./Navbar.sass";
 import { Link } from "react-scroll";
 import { ABOUT_US, ABOUT_US_ID, FAQ, FAQ_ID, HOME_PAGE, HOW_IT_WORKS, HOW_IT_WORKS_ID, PRICING, PRICING_ID } from "../../Constants/routes";
+import useIsSmallScreen from "../../Helpers/useIsSmallScreen";
 
 const Navbar = () => {
+    const isSmallScreen = useIsSmallScreen();
 
     return (
         <div className="navigation-bar">
@@ -12,30 +14,43 @@ const Navbar = () => {
                     <div className="logo-part">
                         <img className="logo" src="/img/logo.svg" alt="main logo"/>
                     </div>
-                    <div className="tab active">
-                        <Link to={ABOUT_US_ID} smooth={true} duration={500}>
-                            <div className="section">About Us</div>
-                        </Link>
-                    </div>
-                    <div className="tab">
-                        <Link to={HOW_IT_WORKS_ID} smooth={true} duration={500}>
-                            <div className="section">How it works</div>
-                        </Link>
-                    </div>
-                    <div className="tab">
-                        <Link to={PRICING_ID} smooth={true} duration={500}>
-                            <div className="section">Pricing</div>
-                        </Link>
-                    </div>
-                    <div className="tab">
-                        <Link to={FAQ_ID} smooth={true} duration={500}>
-                            <div className="section">FAQs</div>
-                        </Link>
-                    </div>
+                    {
+                        !isSmallScreen
+                            ?   <>
+                                    <div className="tab active">
+                                        <Link to={ABOUT_US_ID} smooth={true} duration={500}>
+                                            <div className="section">About Us</div>
+                                        </Link>
+                                    </div>
+                                    <div className="tab">
+                                        <Link to={HOW_IT_WORKS_ID} smooth={true} duration={500}>
+                                            <div className="section">How it works</div>
+                                        </Link>
+                                    </div>
+                                    <div className="tab">
+                                        <Link to={PRICING_ID} smooth={true} duration={500}>
+                                            <div className="section">Pricing</div>
+                                        </Link>
+                                    </div>
+                                    <div className="tab">
+                                        <Link to={FAQ_ID} smooth={true} duration={500}>
+                                            <div className="section">FAQs</div>
+                                        </Link>
+                                    </div>
+                                </>
+                            :   null
+                    }
+
                 </div>
                 <div className="right-buttons flex-row">
-                    <div className="button white">Login</div>
-                    <div className="button black without-margin">Sign Up</div>
+                    {
+                        !isSmallScreen
+                            ?   <>
+                                    <div className="button white">Login</div>
+                                    <div className="button black without-margin">Sign Up</div>
+                                </>
+                            :   null
+                    }
                 </div>
             </div>
         </div>
