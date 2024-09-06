bf55c9e67bcc3cc62dd3f9a8abae2854b8142083
krAlena <krasnoshchoka@bk.ru>
2024-09-06 12:49:30 +0300

    clean

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import './App.sass';
 import Footer from './Components/Footer/Footer';
 import CTA from './Components/CTA/CTA';
 import AboutUs from './Components/AboutUs/AboutUs';
-import { ABOUT_US, CTA_PAGE } from './Constants/routes';
+// import { ABOUT_US, CTA_PAGE } from './Constants/routes';
 // Import components for routing from react-router-dom library
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HowItWorks from './Components/HowItWorks/HowItWorks';
 import Pricing from './Components/Pricing/Pricing';
 import FAQ from './Components/FAQ/FAQ';
@@ -15,9 +15,9 @@ function App() {
   const [theme, setTheme] = useState('light');
   const [isAppScrollLocked, setIsAppScrollLocked] = useState(false);
 
-  const handleThemeChange = (newTheme) => {
-    setTheme(newTheme);
-  };
+  // const handleThemeChange = (newTheme) => {
+  //   setTheme(newTheme);
+  // };
 
   return (
     <div className={!isAppScrollLocked ? `app theme--${theme}` : `app locked-scroll theme--${theme}`}>
