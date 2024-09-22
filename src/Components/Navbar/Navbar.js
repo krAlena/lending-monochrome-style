44954d8e7781b11a2e5b3ace24a3c3d63c1a9584
krAlena <krasnoshchoka@bk.ru>
2024-09-22 22:49:14 +0300

    add change theme icon and convert text color (first iteration)

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from "react-scroll";
 import { ABOUT_US_ID, FAQ_ID,  HOW_IT_WORKS_ID, PRICING_ID } from "../../Constants/routes";
 import useIsSmallScreen from "../../Helpers/useIsSmallScreen";
 import { COLOR_THEME } from "../../Constants/themes";
+import SunMoonSvgIcon from "../Common/Icons/SunMoonSVGIcon";
 
 const Navbar = ({openMobNavbar, currentTheme, changeTheme}) => {
     const isSmallScreen = useIsSmallScreen();
@@ -56,30 +57,14 @@ const Navbar = ({openMobNavbar, currentTheme, changeTheme}) => {
 
                 </div>
                 <div className="right-buttons flex-row">
-                    {/* <div id="theme-toggle"
+                    <div id="theme-toggle"
                         className="theme-toggle" style={{"zIndex" : "1000", "cursor" : "pointer"}}
                         onClick={event => (currentTheme === COLOR_THEME.light)  ? changeTheme(COLOR_THEME.dark) : changeTheme(COLOR_THEME.light)}
                         data-theme={currentTheme}
                         aria-label={currentTheme}
                     >
-                        <svg class="sun-and-moon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
-                            <mask class="moon" id="moon-mask">
-                            <rect x="0" y="0" width="100%" height="100%" fill="white" />
-                            <circle cx="24" cy="10" r="6" fill="black" />
-                            </mask>
-                            <circle class="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="currentColor" />
-                            <g class="sun-beams" stroke="currentColor">
-                            <line x1="12" y1="1" x2="12" y2="3" />
-                            <line x1="12" y1="21" x2="12" y2="23" />
-                            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
-                            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
-                            <line x1="1" y1="12" x2="3" y2="12" />
-                            <line x1="21" y1="12" x2="23" y2="12" />
-                            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
-                            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
-                            </g>
-                        </svg>
-                    </div> */}
+                        <SunMoonSvgIcon className="icon"></SunMoonSvgIcon>
+                    </div>
                     {
                         !isSmallScreen
                             ?   <>
